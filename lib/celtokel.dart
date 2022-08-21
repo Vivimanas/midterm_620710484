93ca9d177f129e39ace745280fe4c09d98384c69
Manassawee Dissabong <dissabong_m@silpakorn.edu>
2022-08-21 12:05:20 +0700

    edit

---
+++ lib/celtokel.dart
@@ -0,0 +1,8 @@
+class temp2{
+
+  int celtokel(double c, double k){
+    return (k = c + 273.15).round();
+  }
+
+
+}
---
+++ lib/fahtokel.dart
@@ -0,0 +1,8 @@
+class temp4{
+
+  int fahtokel(double f, double k){
+    return (k = (f + 459.67) / 1.8).round();
+  }
+
+
+}
